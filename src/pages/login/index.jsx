db1a9186cf0bd2529648ed06af0816e7021abf36
wangchengjiang <1312975595@qq.com>
2020-06-01 15:30:55 +0800

    m登录

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,20 +1,68 @@
 import Taro, { Component } from "@tarojs/taro";
-import { View, Button, Text } from "@tarojs/components";
+import { View, Text } from "@tarojs/components";
 import "./index.scss";
-import { AtTabs, AtTabsPane } from "taro-ui";
-
+import {
+  AtTabs,
+  AtTabsPane,
+  AtInput,
+  AtForm,
+  AtButton,
+  AtToast
+} from "taro-ui";
+function check_phone(phoneNum) {
+  return /^1[3456789]\d{9}$/.test(phoneNum); //true
+}
 class Index extends Component {
   config = {
     navigationBarTitleText: "登录"
   };
   state = {
-    current: 0
+    current: 0,
+    phone: "",
+    password: "",
+    showToast: false, //轻提示
+    text: "" //提示内容
   };
   handleClick(value) {
     this.setState({
       current: value
     });
   }
+  handleChangePhone = value => {
+    this.setState({
+      phone: value
+    });
+  };
+  handleChangePassword = value => {
+    this.setState({
+      password: value
+    });
+  };
+  handelLogin = () => {
+    let isAbled = check_phone(this.state.phone);
+    if (this.state.phone) {
+      if (isAbled) {
+        //输入格式不正确的情况
+      } else {
+        this.setState({
+          showToast: true,
+          text: "手机格式不正确"
+        });
+      }
+    } else {
+      //输入为空的情况
+      this.setState({
+        showToast: true,
+        text: "请输入手机号码"
+      });
+    }
+  };
+  closeToast = () => {
+    this.setState({
+      showToast: false, //轻提示
+      text: "" //提示内容
+    });
+  };
   componentWillReceiveProps(nextProps) {
     console.log(this.props, nextProps);
   }
@@ -40,17 +88,64 @@ class Index extends Component {
             animated={false}
           >
             <AtTabsPane current={this.state.current} index={0}>
-              <View style="padding: 100px 50px;background-color: #FAFBFC;text-align: center;">
-                标签页一的内容
+              <View>
+                <View className="phoneInput">
+                  <View className="phoneTitle">86+</View>
+                  <AtInput
+                    name="value6"
+                    border={false}
+                    type="phone"
+                    placeholder="请输入手机号码"
+                    value={this.state.phone}
+                    onChange={this.handleChangePhone}
+                  />
+                </View>
+                <View className="passwordInput">
+                  <AtInput
+                    name="value3"
+                    type="password"
+                    placeholder="请输入密码"
+                    value={this.state.password}
+                    onChange={this.handleChangePassword}
+                  />
+                </View>
+                <AtButton className="login" onClick={this.handelLogin}>
+                  登录
+                </AtButton>
               </View>
             </AtTabsPane>
             <AtTabsPane current={this.state.current} index={1}>
-              <View style="padding: 100px 50px;background-color: #FAFBFC;text-align: center;">
-                标签页二的内容
+              <View>
+                <View className="phoneInput">
+                  <AtInput
+                    name="value6"
+                    border={false}
+                    type="phone"
+                    placeholder="请输入邮箱"
+                    value={this.state.phone}
+                    onChange={this.handleChangePhone}
+                  />
+                </View>
+                <View className="passwordInput">
+                  <AtInput
+                    name="value3"
+                    type="password"
+                    placeholder="请输入密码"
+                    value={this.state.password}
+                    onChange={this.handleChangePassword}
+                  />
+                </View>
+                <AtButton className="login">登录</AtButton>
               </View>
             </AtTabsPane>
           </AtTabs>
         </View>
+        <AtToast
+          isOpened={this.state.showToast}
+          text={this.state.text}
+          duration={1000}
+          onClose={this.closeToast}
+        ></AtToast>
       </View>
     );
   }
